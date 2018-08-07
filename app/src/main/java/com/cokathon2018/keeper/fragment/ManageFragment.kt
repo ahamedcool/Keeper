b267d77a748e7b80802856670b6cb4e016788c89
woohyunjin06 <woohyunjin06@naver.com>
2018-08-07 20:41:47 +0900

    Remove useless part

---
+++ app/src/main/java/com/cokathon2018/keeper/fragment/ManageFragment.kt
@@ -0,0 +1,44 @@
+package com.cokathon2018.keeper.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cokathon2018.keeper.util.PreferencesUtils
+import com.cokathon2018.keeper.R
+import com.cokathon2018.keeper.`interface`.OnClickListener
+import com.cokathon2018.keeper.adapter.ManageAdapter
+import com.cokathon2018.keeper.adapter.ManageItem
+import kotlinx.android.synthetic.main.fragment_manage.view.*
+
+class ManageFragment : Fragment(), OnClickListener{
+
+    override fun onClick(pos: Int) {
+
+    }
+    private var adapter : ManageAdapter? = null
+    private var mItems = ArrayList<ManageItem>()
+    private var v : View? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        v = inflater.inflate(R.layout.fragment_manage, container, false)
+
+        initRecyclerView()
+        val strUsrlist = PreferencesUtils(activity!!).getData("user")
+        val usrlist = strUsrlist.split(",")
+        if(strUsrlist.isNotEmpty())
+            for(usr : String in usrlist){
+                mItems.add(ManageItem(usr,"18.8.7 18:15"))
+            }
+        return v
+    }
+    private fun initRecyclerView() {
+        // 변경될 가능성 o : false 로 , 없다면 true.
+        v!!.recyclerView.setHasFixedSize(false)
+
+        adapter = ManageAdapter(mItems, this)
+        v!!.recyclerView.adapter = adapter
+        v!!.recyclerView.layoutManager = LinearLayoutManager(activity!!)
+    }
+}
