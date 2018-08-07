251095231503384b3aa53c65cc582fae8f00f7dc
woohyunjin06 <woohyunjin06@naver.com>
2018-08-07 15:35:57 +0900

    Initial commit

---
+++ app/src/main/java/com/cokathon2018/keeper/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cokathon2018.keeper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
